3f2c3703bae94ecf60b112d8cbdc9ca2bb7516ea
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-11-10 21:51:10 -0600

    update urls.py from profile

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path
 from django.conf.urls import include
-from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from .views import ProfileUserModelView
 
 urlpatterns = [
-    re_path(r'^profileModel_url', ProfileUserModelView.as_view())
+    re_path(r'^', ProfileUserModelView.as_view())
 ]
