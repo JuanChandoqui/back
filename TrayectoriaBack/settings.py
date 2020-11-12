956d95eec47338faa0265ec4cdfe70cde7caf93c
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-11-11 21:58:05 -0600

    add connection with postgres heroku

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from decouple import Csv
 import dj_database_url
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,6 +108,8 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+
+psycopg2.connect(config('DATABASE_URL'), sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
