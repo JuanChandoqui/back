e1197e055cebc2164961cbc71159e021aeffb1a0
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-11-12 14:15:48 -0600

    delete app Profile

--- Profile/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework  import serializers
-
-# Modelos
-from Profile.models import ProfileUser
-
-class ProfileUserModelSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = ProfileUser
-        fields = ('__all__')
--- Profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from django.conf.urls import include
-from rest_framework import routers, serializers, viewsets
-from Profile import views
-
-urlpatterns = [
-    #re_path(r'^profileUser_url', views.ProfileUserModelView.as_view()),
-    re_path(r'^profileModel_url', views.ProfileUserModelView.as_view())
-]
