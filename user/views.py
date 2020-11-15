e75869f5d9b7c0695180182c42a901b6190ec2df
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-11-14 18:15:23 -0600

    add method put from usermodel

--- user/views.py
+++ user/views.py
@@ -28,6 +28,19 @@ class UserModelView(ObtainAuthToken):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, *args, **kwargs):
+        try:
+            user = self.kwargs.get('pk')
+        except user.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserModelSerializers(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserProfileView(ObtainAuthToken):
     #GET REQUEST
