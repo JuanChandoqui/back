ab66846a8fc994202a1e2882813101e87c5150da
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-11-30 14:43:20 -0600

    update run-dev

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated'),
     'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication'),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     "DEFAULT_GENERATOR_CLASS": "rest_framework.schemas.generators.BaseSchemaGenerator",
